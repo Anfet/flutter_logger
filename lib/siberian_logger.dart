da03be92689af3f431a29559fa5751b8af579dd6
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2024-04-28 15:55:14 +0300

    * добавлен экспорт логгера, intl и sprintf

--- lib/siberian_logger.dart
+++ lib/siberian_logger.dart
@@ -1,4 +1,9 @@
 library siberian_logger;
 
+export 'package:logger/logger.dart';
+export 'package:intl/intl.dart';
+export 'package:sprintf/sprintf.dart';
+
 export 'src/logger.dart';
 export 'src/mixins/logging.dart';
+
