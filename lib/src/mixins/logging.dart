249704866ef0c1808aaac9c29482a35a7e7da084
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2024-04-29 22:08:36 +0300

    * функции лога теперь protected

--- lib/src/mixins/logging.dart
+++ lib/src/mixins/logging.dart
@@ -1,17 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 import 'package:siberian_logger/src/logger.dart';
 
 mixin Logging {
 
+  @protected
   void log(message, {String? tag, Level level = Level.trace, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
       logMessage(message, level: level, tag: tag ?? "$runtimeType", error: error, stack: stack);
 
+  @protected
   void warn(message, {String? tag, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
       logMessage(message, level: Level.warning, tag: tag ?? "$runtimeType", error: error, stack: stack);
 
+  @protected
   void trace(message, {String? tag, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
       logMessage(message, level: Level.trace, tag: tag ?? "$runtimeType", error: error, stack: stack);
 
+  @protected
   void recordException(message, [Object? error, StackTrace? stack, String? tag, bool truncateMessage = true]) =>
       logMessage(message, level: Level.error, tag: tag ?? "$runtimeType", error: error, stack: stack);
 }
