a3f0ea267afa512fa15338cc0150c8768ac5ac56
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2026-01-11 18:05:37 +0200

    - mixin global tag now has a getter

--- lib/src/mixins/logging.dart
+++ lib/src/mixins/logging.dart
@@ -4,19 +4,21 @@ import 'package:talker/talker.dart';
 import '../logger.dart';
 
 mixin Logging {
+  String get TAG => '$runtimeType';
+
   @protected
   void log(message, {String? tag, LogLevel level = LogLevel.verbose, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
-      logMessage(message, level: level, error: error, stack: stack, tag: tag ?? '$runtimeType');
+      logMessage(message, level: level, error: error, stack: stack, tag: tag ?? TAG);
 
   @protected
   void warn(message, {String? tag, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
-      logMessage(message, level: LogLevel.warning, error: error, stack: stack, tag:  tag ?? '$runtimeType');
+      logMessage(message, level: LogLevel.warning, error: error, stack: stack, tag: tag ?? TAG);
 
   @protected
   void trace(message, {String? tag, Object? error, StackTrace? stack, bool truncateMessage = true}) =>
-      logMessage(message, level: LogLevel.verbose, error: error, stack: stack, tag:  tag ?? '$runtimeType');
+      logMessage(message, level: LogLevel.verbose, error: error, stack: stack, tag: tag ?? TAG);
 
   @protected
   void recordException(message, [Object? error, StackTrace? stack, String? tag, bool truncateMessage = true]) =>
-      logMessage(message, level: LogLevel.error, error: error, stack: stack, tag:  tag ?? '$runtimeType');
+      logMessage(message, level: LogLevel.error, error: error, stack: stack, tag: tag ?? TAG);
 }
